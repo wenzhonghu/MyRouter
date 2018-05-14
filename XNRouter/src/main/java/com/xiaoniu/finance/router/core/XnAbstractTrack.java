3cb2d0e4523ff56e85ddd671405518f87892ebd2
wenzhonghu <wenzhonghu@qq.com>
2018-05-14 14:57:08 +0800

    docs:修改核心接口的文档描述

--- XNRouter/src/main/java/com/xiaoniu/finance/router/core/XnAbstractTrack.java
+++ XNRouter/src/main/java/com/xiaoniu/finance/router/core/XnAbstractTrack.java
@@ -14,7 +14,7 @@ public abstract class XnAbstractTrack {
     /**
      * 业务调用,支持参数传递
      * 同时支持单向的对象传递,方法如下
-     * new XnRouterResult.Builder().code(CODE_SUCCESS).msg(xxx).object(new FundHomeFragment()).build()
+     * return XnRouterResult.ok(new FundHomeFragment())
      *
      * @param context
      * @param requestData 请求参数组
@@ -24,17 +24,17 @@ public abstract class XnAbstractTrack {
 
     /**
      * 业务调用,支持参数传递同时提供对象互相传递
-     * 只有当XnRouter在调用了object(对象)才会被调用此方法
+     * 只有当XnRouter在调用了object(对象object)才会被调用此方法
      * eg:
-     * if (object instanceof TextView) {
-     * ((TextView) object).setText("The text is param me.");
-     * Toast returnToast = Toast.makeText(context, "I am returned Toast.", Toast.LENGTH_SHORT);
-     * return new XnRouterResult.Builder().code(CODE_SUCCESS).msg(xxx).object(returnToast).build();
+     * if (object instanceof Test) { //此处为调用方传递过来的对象引用
+     * ((Test) object).setText("The object is param.");
+     * Test returnTest = new Test("new test");
+     * return XnRouterResult.ok(returnTest);//回调给调用方的对象引用
      * }
      *
      * @param context
      * @param requestData 请求参数组
-     * @param object      传递对应的对象过去
+     * @param object      传递对应的对象过去,不支持跨进程对称传递
      * @return
      */
     public XnRouterResult fire(Context context, Bundle requestData, Object object) {
