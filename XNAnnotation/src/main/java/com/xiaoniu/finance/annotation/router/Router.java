b6511b8be3f6e956555233cfbed7397f508b6f4e
wenzhonghu <wenzhonghu@qq.com>
2018-05-14 15:49:42 +0800

    docs:修改核心接口的文档描述

--- XNAnnotation/src/main/java/com/xiaoniu/finance/annotation/router/Router.java
+++ XNAnnotation/src/main/java/com/xiaoniu/finance/annotation/router/Router.java
@@ -6,15 +6,48 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * 实现路由注解模式
  * Created by wenzhonghu on 2018/4/27.
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
 public @interface Router {
+    /**
+     * 路由路径
+     * @return
+     */
     String value();
+
+    /**
+     * 是否禁用
+     * @return
+     */
     boolean enable() default true;
+
+    /**
+     * 权限配置
+     * @see(PermissionType)
+     * @return
+     */
     int permission() default 0;
+
+    /**
+     * 是否支持跨进程
+     * @return
+     */
     boolean crossable() default false;
+
+    /**
+     * 路由路径的配置规则
+     * @return
+     */
     //String expression() default "";
+
+    /**
+     * 路由路径的配置开关
+     * 默认为true,表示全路径完全匹配
+     * 如果false,则可以通过expression参数进行部分匹配操作
+     * @return
+     */
     boolean match() default true;
 }
