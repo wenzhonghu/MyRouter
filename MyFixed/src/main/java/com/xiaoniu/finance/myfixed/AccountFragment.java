ee97f3bd41ddff518660eac8cd57c30e714a9363
wenzhonghu <wenzhonghu@qq.com>
2018-05-15 15:00:27 +0800

    test:完善测试代码

--- MyFixed/src/main/java/com/xiaoniu/finance/myfixed/AccountFragment.java
+++ MyFixed/src/main/java/com/xiaoniu/finance/myfixed/AccountFragment.java
@@ -11,9 +11,6 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AccountFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link AccountFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
@@ -26,8 +23,6 @@ public class AccountFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -67,13 +62,6 @@ public class AccountFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -82,21 +70,5 @@ public class AccountFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
